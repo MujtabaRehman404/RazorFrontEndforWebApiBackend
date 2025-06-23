7e08aa048fea7e2a864bd07c760e1537c8539fda
Mujtaba Rehman <mujtubasteam0333@gmail.com>
2025-06-23 10:23:25 -0700

    done

---
+++ EmployeeFrontEnd/Pages/testpage/testapi.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmployeeFrontEnd.Pages.testpage
+{
+    public class testapiModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
